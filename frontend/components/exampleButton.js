c3e1de0fb86fb0eeaf990ab792884871c754c12e
Robin Verton <hello@robinverton.de>
2022-01-18 21:40:47 +0100

    improve initial docs page

---
+++ frontend/components/exampleButton.js
@@ -0,0 +1,5 @@
+export default function ExampleButton({ children }) {
+  return (
+    <span className="py-1 px-2 border rounded text-sm">{children}</span>
+  )
+}
---
+++ frontend/components/highlightWord.js
@@ -0,0 +1,5 @@
+export default function HighlightWord({ children }) {
+  return (
+    <span className="text-yellow-600">{children}</span>
+  )
+}
